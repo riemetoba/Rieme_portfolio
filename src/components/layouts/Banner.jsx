127cf35a37a43d1a671af02b046f03992f66840c
Toba <riemehasan06@gmail.com>
2026-02-20 23:59:10 +0600

    f

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
     <Container>
         <Flex className={"justify-between"}>
             <div className="">
-                <h3 className="text-2xl font-inter font-medium">Hello there</h3>
+                <h3 className="md:text-2xl font-inter font-medium">Hello there</h3>
                 <h1 className="text-6xl font-inter font-bold py-4">Rieme Hasan</h1>
                 <p className="text-[20px] font-inter font-medium">I am a Frontend Developer</p>
                 <p className="text-base font-inter leading-6 py-6 text-[#555A64]">Bringing designs to life with clean code and modern frontend technologies.</p>
