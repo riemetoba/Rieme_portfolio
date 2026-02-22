b39e6460a760800113a1d4b0d3fa1d2796b30b98
Toba <riemehasan06@gmail.com>
2026-02-22 13:37:21 +0600

    banner responsive fixed

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -6,14 +6,14 @@ import Flex from "../Flex"
 
 const Banner = () => {
   return (
-   <div className="bg-white py-10 md:py-25">
+   <div className="bg-white py-10 lg:py-25">
     <Container>
         <Flex className={"justify-between"}>
-            <div className="ml-2">
-                <h3 className="pt-6 text-2xl font-inter font-medium">Hello there</h3>
-                <h1 className="text-6xl font-inter font-bold py-4">Rieme Hasan</h1>
-                <p className="text-[20px] font-inter font-medium">I am a Frontend Developer</p>
-                <p className="text-base font-inter leading-6 py-6 text-[#555A64]">Bringing designs to life with clean code and modern frontend technologies.</p>
+            <div className="px-5 lg:px-0">
+                <h3 className="pt-6 text-xl lg:text-3xl font-inter font-medium">Hello there</h3>
+                <h1 className="text-4xl lg:text-6xl font-inter font-bold py-4">Rieme Hasan</h1>
+                <p className="text-lg lg:text-2xl font-inter font-medium">I am a Frontend Developer</p>
+                <p className="text-sm lg:text-base font-inter leading-relaxed py-6 text-[#555A64]">Bringing designs to life with clean code and modern frontend technologies.</p>
                 <div className="flex items-center gap-x-5">
                      <button className="py-3.5 px-6 mt-9 bg-org text-[#ffffff] font-inter font-medium uppercase hover:text-white hover:bg-black duration-300">My Work</button>
                      <button className="py-3 px-7 mt-9 border-2 border-org text-org font-inter font-medium uppercase hover:text-white hover:bg-org duration-300">Hire Me</button>
