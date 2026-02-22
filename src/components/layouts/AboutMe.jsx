60362f1ae7065ed1ee712be951fbdc7df2d82be0
Toba <riemehasan06@gmail.com>
2026-02-22 13:24:17 +0600

    about section responsive

--- src/components/layouts/AboutMe.jsx
+++ src/components/layouts/AboutMe.jsx
@@ -1,54 +1,111 @@
 import { Link } from "react-router-dom";
-import Container from "../Container"
-import Flex from "../Flex"
-import Image from "../Image"
-import aboutFake from "/src/assets/aboutFake.png"
+import Container from "../Container";
+import Flex from "../Flex";
+import Image from "../Image";
+import aboutFake from "/src/assets/aboutFake.png";
 import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
-
-
-
 
 const AboutMe = () => {
   return (
     <div className="bg-[#EAEDF2] py-25">
       <Container>
-         <h3 className="font-inter text-2xl md:text-3xl font-black ml-2 relative">ABOUT ME <span className="absolute left-0 -bottom-2 w-9 h-0.5 bg-org"></span></h3>
-        <p className="font-inter uppercase text-sm text-[#555A64] pt-5 mb-10 ml-2">A passionate front-end developer based in bangladesh</p>
-        <div className={"md:justify-between flex md:flex-row flex-col mt-20"}>
-          {/* Image er jonno  */}
-         <div className="border-white border-12 rounded relative">
-          <Image src={aboutFake}/>
-         <Link to={"https://github.com/riemetoba"}>
-          <div className="absolute flex items-center justify-center w-10 h-12 md:w-13 md:h-15 bg-org top-8 left-0 border-white border-b-10 border-r-10 border-t-10"><FaGithub className="text-[22px]  text-white"/></div>
-         </Link>
-         <Link to={"https://www.facebook.com/profile.php?id=100016533453052"}>
-          <div className="absolute flex items-center justify-center w-10 h-12 md:w-13 md:h-15 bg-org top-20 left-0 border-white border-b-10 border-r-10 border-t-10"><FaFacebook className="text-[22px]  text-white"/></div>
-         </Link>
-         <Link to={"https://www.linkedin.com/in/rieme-hasan-291767398"}>
-          <div className="absolute flex items-center justify-center w-10 h-12 md:w-13 md:h-15 bg-org top-32 left-0 border-white border-b-10 border-r-10 border-t-10"><FaLinkedin className="text-[22px]  text-white"/></div>
-         </Link>
-         </div>
-          {/* Image er jonno  */}
-          <div className="w-98 mt-4 ml-2">
-            <h3 className="text-2xl font-inter font-bold">I'M Rieme Hasan</h3>
-            <p className="md:text-[20px] text-base font-inter pt-3 pb-6">A Passionate <span className="text-org">Front-End</span> Developer Based in <span className="text-org"> Bangladesh</span></p>
-            <p className="text-[#555A64] text-base font-inter w-150 leading-6.5">I’m a frontend developer who enjoys turning Figma designs into clean, responsive, and engaging user interfaces. I work with HTML, Bootstrap, Tailwind CSS, and React.js to build modern, component-based web experiences. I care deeply about visual accuracy, usability, and writing maintainable code that brings designs to life. Check out my Portfolio</p>
-             <div className="flex items-center gap-x-12 mt-10">
-            <div className="">
-              <p className="text-sm font-inter pb-4">Birthday <span className="ml-6 text-[#CCCCCC]">|</span> <span className="ml-4 text-[#555A64]">26th December 1997</span></p>
-              <p className="text-sm font-inter pb-4">Address <span className="ml-6 text-[#CCCCCC]">|</span> <span className="ml-4 text-[#555A64]">Dhaka, Bangladesh</span></p>
+        {/* Section title  */}
+        <h3 className="font-inter text-2xl md:text-3xl font-black inline-block relative px-5 lg:px-0">
+          ABOUT ME
+          <span className="absolute left-5 -bottom-2 lg:left-0 w-9 h-0.5 bg-org"></span>
+        </h3>
+        <p className="font-inter uppercase text-sm text-[#555A64] pt-5 mb-10 px-5 lg:px-0">
+          A passionate front-end developer based in bangladesh
+        </p>
+        {/* Flex start  */}
+        <div className={"flex lg:flex-row flex-col lg:items-center gap-12 lg:justify-between px-5 lg:px-0"}>
+          {/* Left side start  */}
+          {/* Image section  */}
+          <div className="bg-[#ffffff] p-4 max-w-sm w-full rounded relative">
+            <Image src={aboutFake} className={"w-full h-auto"} />
+            {/* Image icons  */}
+            <div className="absolute left-4 top-8 flex flex-col">
+              <Link to="https://github.com/riemetoba" target="_blank">
+                <div className="w-10 h-12 bg-org flex items-center justify-center border-white border-y-4 border-r-4">
+                  <FaGithub className="text-white text-lg" />
+                </div>
+              </Link>
+
+              <Link
+                to="https://www.facebook.com/profile.php?id=100016533453052"
+                target="_blank"
+              >
+                <div className="w-10 h-12 bg-org flex items-center justify-center border-white border-y-4 border-r-4 mt-2">
+                  <FaFacebook className="text-white text-lg" />
+                </div>
+              </Link>
+
+              <Link
+                to="https://www.linkedin.com/in/rieme-hasan-291767398"
+                target="_blank"
+              >
+                <div className="w-10 h-12 bg-org flex items-center justify-center border-white border-y-4 border-r-4 mt-2">
+                  <FaLinkedin className="text-white text-lg" />
+                </div>
+              </Link>
             </div>
-            <div className="">
-              <p className="text-sm font-inter pb-4">E-mail <span className="ml-6 text-[#CCCCCC]">|</span> <span className="ml-4 text-[#555A64]">riemehasan06@gmail.com</span></p>
-              <p className="text-sm font-inter pb-4">Phone <span className="ml-6 text-[#CCCCCC]">|</span> <span className="ml-4 text-[#555A64]">01577043696</span></p>
+            {/* Image icons  */}
+          </div>
+          {/* Image section  */}
+          {/* Left side end  */}
+          {/* Right side start  */}
+          <div className="w-full max-w-xl px-5 lg:px-o">
+            <h3 className="text-xl md:text-2xl font-inter font-bold">
+              I'M Rieme Hasan
+            </h3>
+            <p className="text-xl font-inter mt-3 mb-6">
+              A Passionate <span className="text-org">Front-End</span> Developer
+              Based in <span className="text-org"> Bangladesh</span>
+            </p>
+            <p className="text-[#555A64] text-base font-inter md:w-150 leading-relaxed">
+              I’m a frontend developer who enjoys turning Figma designs into
+              clean, responsive, and engaging user interfaces. I work with HTML,
+              Bootstrap, Tailwind CSS, and React.js to build modern,
+              component-based web experiences. I care deeply about visual
+              accuracy, usability, and writing maintainable code that brings
+              designs to life. Check out my Portfolio
+            </p>
+            {/* Information part  */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-6 mt-8 text-sm">
+              <div className="">
+                <p className="text-sm font-inter pb-4">
+                  Birthday <span className="ml-6 text-[#CCCCCC]">|</span>{" "}
+                  <span className="ml-4 text-[#555A64]">
+                    26th December 1997
+                  </span>
+                </p>
+                <p className="text-sm font-inter pb-4">
+                  Address <span className="ml-6 text-[#CCCCCC]">|</span>{" "}
+                  <span className="ml-4 text-[#555A64]">Dhaka, Bangladesh</span>
+                </p>
+              </div>
+              <div className="">
+                <p className="text-sm font-inter pb-4">
+                  E-mail <span className="ml-9 md:ml-6 text-[#CCCCCC]">|</span>{" "}
+                  <span className="ml-4 text-[#555A64]">
+                    riemehasan06@gmail.com
+                  </span>
+                </p>
+                <p className="text-sm font-inter pb-4">
+                  Phone <span className="ml-9 md:ml-6 text-[#CCCCCC]">|</span>{" "}
+                  <span className="ml-4 text-[#555A64]">01577043696</span>
+                </p>
+              </div>
             </div>
+            <button className="py-3 px-6 mt-9 bg-org text-[#ffffff] font-inter font-medium uppercase hover:text-white hover:bg-black duration-300">
+              Download CV
+            </button>
           </div>
-         <button className="py-3 px-6 mt-9 bg-org text-[#ffffff] font-inter font-medium uppercase hover:text-white hover:bg-black duration-300">Download CV</button>
-          </div>
+          {/* Right side end  */}
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
