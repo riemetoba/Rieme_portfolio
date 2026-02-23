30012fd093d780e2bb5c29fc17cc2385ba2a90f9
Toba <riemehasan06@gmail.com>
2026-02-23 22:13:23 +0600

    skill section responsive

--- src/components/layouts/Skill.jsx
+++ src/components/layouts/Skill.jsx
@@ -23,36 +23,36 @@ const Skill = () => {
   return (
     <div className="py-25">
       <Container>
-        <h3 className="font-inter text-3xl font-black relative">
+        <h3 className="font-inter text-2xl lg:text-3xl px-5 lg:px-0 font-black relative">
           MY SKILLS{" "}
-          <span className="absolute left-0 -bottom-2 w-9 h-0.5 bg-org"></span>
+          <span className="absolute left-5 lg:left-0 -bottom-2 w-9 h-0.5 bg-org"></span>
         </h3>
-        <p className="font-inter uppercase text-sm text-[#555A64] pt-5 mb-10">
+        <p className="font-inter uppercase text-sm text-[#555A64] pt-5 mb-10 px-5 lg:px-0">
           A passionate front-end developer based in bangladesh
         </p>
         {/* shadCN  */}
-        <Field className="w-full max-w-sm">
+        <Field className="w-full max-w-sm px-5 lg:px-0">
           <FieldLabel htmlFor="progress-upload">
             <span className="font-inter text-[18px]">HTML</span>
             <span className="font-inter text-sm text-[#555A64] ml-66">90%</span>
           </FieldLabel>
           <Progress value={html} />
         </Field>
-        <Field className="w-full max-w-sm">
+        <Field className="w-full max-w-sm px-5 lg:px-0">
           <FieldLabel htmlFor="progress-upload">
             <span className="font-inter text-[18px]">CSS</span>
             <span className="ml-67 font-inter text-sm text-[#555A64]">86%</span>
           </FieldLabel>
           <Progress value={css} />
         </Field>
-        <Field className="w-full max-w-sm">
+        <Field className="w-full max-w-sm px-5 lg:px-0">
           <FieldLabel htmlFor="progress-upload">
             <span className="font-inter text-[18px]">JAVASRCIPT</span>
             <span className="ml-28 font-inter text-sm text-[#555A64]">65%</span>
           </FieldLabel>
           <Progress value={js} />
         </Field>
-        <Field className="w-full max-w-sm">
+        <Field className="w-full max-w-sm px-5 lg:px-0">
           <FieldLabel htmlFor="progress-upload">
             <span className="font-inter text-[18px]">REACT</span>
             <span className="ml-54 font-inter text-sm text-[#555A64]">80%</span>
