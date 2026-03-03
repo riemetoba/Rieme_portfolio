4404525cace27e2fc4cd4dea1e5a83d3af3bf52b
Toba <riemehasan06@gmail.com>
2026-03-03 23:45:45 +0600

    fix 2

--- src/components/layouts/Service.jsx
+++ src/components/layouts/Service.jsx
@@ -21,35 +21,37 @@ const Service = () => {
         <p className="font-inter uppercase text-sm text-[#555A64] pt-5 mb-10 px-5 lg:px-0">
           A passionate front-end developer based in bangladesh
         </p>
-        <Flex className={"justify-between"}>
-            <div className="w-85 h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
+        {/* Flex start  */}
+        <Flex className={"lg:justify-between flex-col gap-y-2 lg:gap-y-0"}>
+            <div className="mx-5 lg:mx-0 md:w-85 md:h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
                 <Image className={"group-hover:brightness-0 group-hover:invert duration-300"} src={servOne}/>
                 <h3 className="my-5 font-inter text-[18px] group-hover:text-white duration-300">Figma to Code</h3>
                 <p className="font-inter text-base text-[#555A64] group-hover:text-white duration-300">I Convert Figma designs into clean, pixel-perfect frontend code.</p>
             </div>
-            <div className="w-85 h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
+            <div className="mx-5 lg:mx-0 md:w-85 md:h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
                 <Image className={"group-hover:brightness-0 group-hover:invert duration-300"} src={servTwo}/>
                 <h3 className="my-5 font-inter text-[18px] group-hover:text-white duration-300">React Frontend</h3>
                 <p className="font-inter text-base text-[#555A64] group-hover:text-white duration-300">I develop modern and interactive user interfaces using React and clean Javascript.</p>
             </div>
-            <div className="w-85 h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
+            <div className="mx-5 lg:mx-0 md:w-85 md:h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
                 <Image className={"group-hover:brightness-0 group-hover:invert duration-300"} src={servThree}/>
                 <h3 className="my-5 font-inter text-[18px] group-hover:text-white duration-300">API Integration</h3>
                 <p className="font-inter text-base text-[#555A64] group-hover:text-white duration-300">I Integrate backend APIs to display dynamic data and handle real-time interactions.</p>
             </div>
         </Flex>
-        <Flex className={"justify-between"}>
-            <div className="w-85 h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
+        {/* Flex end  */}
+        <Flex className={"lg:justify-between flex-col gap-y-2 lg:gap-y-0"}>
+            <div className="mx-5 lg:mx-0 md:w-85 md:h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
                 <Image className={"group-hover:brightness-0 group-hover:invert duration-300"} src={servFour}/>
                 <h3 className="my-5 font-inter text-[18px] group-hover:text-white duration-300">Responsive Development</h3>
                 <p className="font-inter text-base text-[#555A64] group-hover:text-white duration-300">I build mobile-first, fully responsive websites that look great on all screen sizes.</p>
             </div>
-            <div className="w-85 h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
+            <div className="mx-5 lg:mx-0 md:w-85 md:h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
                 <Image className={"group-hover:brightness-0 group-hover:invert duration-300"} src={servSix}/>
                 <h3 className="my-5 font-inter text-[18px] group-hover:text-white duration-300">Perfomance Optimization</h3>
                 <p className="font-inter text-base text-[#555A64] group-hover:text-white duration-300">I optimize frontend performance to ensure fast loading and smooth user experience.</p>
             </div>
-            <div className="w-85 h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
+            <div className="mx-5 lg:mx-0 md:w-85 md:h-72 bg-white rounded-[5px] mt-8 px-7 py-9 shadow-2xl hover:bg-org duration-300 group">
                 <Image className={"group-hover:brightness-0 group-hover:invert duration-300"} src={servFive}/>
                 <h3 className="my-5 font-inter text-[18px] group-hover:text-white duration-300">Cross-Browser Support</h3>
                 <p className="font-inter text-base text-[#555A64] group-hover:text-white duration-300">I ensure consistent design and functionality across all mordern browsers.</p>
